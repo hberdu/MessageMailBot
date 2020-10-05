78b71cde5496a977006ac4eb9ce9aa01f9ae56c6
hberdu <henriqueberdu@gmail.com>
2020-10-05 19:06:56 -0300

    Commit de Listagem de E-mails
    Commit realizado para adição da funcionalidade de listagem para os e-mails

--- src/main.py
+++ src/main.py
@@ -1,84 +1,51 @@
 import discord
 from discord.message import MessageType
 import os.path
+# Importing libraries 
+import imaplib, email 
+from imap_tools import MailBox
+  
+user = 'eadbotifsp@gmail.com'
+password = 'ifsp1918'
+imap_url = 'imap.gmail.com'  
+ 
+con = imaplib.IMAP4_SSL(imap_url)    
+con.login(user, password)    
+con.select('Inbox')    
 
-emails = list()
+typ, data = con.select('INBOX')
+num_msgs = int(data[0]) - 1
+#print ('Existem %d e-mails na INBOX' % num_msgs)
 
-if os.path.isfile('emails.txt'):
-    with open('emails.txt') as file:
-        for line in file:
-            emails.append(line)
-
-
-def write_emails(mails, *args):
-    for arg in args:
-        mails.remove(arg)
-    for mail in mails:
-        with open('emails.txt','w') as f:
-            f.write(mail+"\n")
-
-def send_email(recipient, body):
-
-    import smtplib
-    from email.mime.text import MIMEText
-    from email.mime.multipart import MIMEMultipart
-
-    print("message: ")
-    print(body+"\n")
-    print("------")
-
-    gmail_user = 'eadbotifsp@gmail.com'
-    gmail_pwd = 'ifsp1918'
-    FROM = 'from'
-    TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = 'subject'
-
-    msg = MIMEMultipart()
-    msg['Subject'] = SUBJECT
-    msg['From'] = FROM
-    msg['To'] = recipient
-    msg.attach(MIMEText(body,'plain'))
-
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(gmail_user, gmail_pwd)
-        server.sendmail(FROM, TO, msg.as_string())
-        server.close()
-        print("successfully sent the mail to "+recipient)
-    except: print("failed to send mail to "+recipient)
+with MailBox('imap.gmail.com').login('eadbotifsp@gmail.com', 'ifsp1918', 'INBOX') as mailbox:
+    subject = [msg.subject for msg in mailbox.fetch()] 
+    sender = [msg.from_ for msg in mailbox.fetch()]  
+    bodies = [msg.text for msg in mailbox.fetch()]   
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print('Logado como :')
     print(client.user.name)
     print(client.user.id)
-    print('------')
-
+    print('------------------')
 
 @client.event
 async def on_message(message):
-    if message.content == "!list":
-        for email in emails:
-            channel = message.channel
-            await channel.send(content=str(subject))
 
-    elif message.content[:4] == "!add":
-        print("Added "+message.content[5:]+" to emails")
-        emails.append(message.content[5:])
-        write_emails(emails)
+     if ".total" in message.content:
+           #await Bot.send_message(message.channel, 'yes')
+        print("Total")
+        channel = message.channel
+        await channel.send(content=str('Existem %d e-mails na INBOX' % num_msgs))
 
-    elif message.content[:4] == "!del":
-        print("Removed "+message.content[5:]+" from emails")
-        write_emails(emails, message.content[5:])
+     elif ".listar" in message.content:
+        print("Lista")
+        channel = message.channel
+        await channel.send(content=str("De:" + " " + sender[num_msgs]))
+        await channel.send(content=str("Assunto:" +  " " + subject[num_msgs]))
+        await channel.send(content=str("Texto:"+ " " + bodies[num_msgs]))       
 
-    elif message.type is MessageType.default:
-        if str(message.author) != 'EAD Bot':
-            for email in emails:
-                send_email(email, message.content)
+client.run('NzYxMTE1ODM3NzA4MzY5OTUw.X3V6XA.V05UNnzCLRoPpPEitGMad3qk-bk')
 
-client.run('NzYxMTE1ODM3NzA4MzY5OTUw.X3V6XA.bM3FEjLcyGeUbLQp3DhY9CWRYu0')
